f01e0216c45e630e899b410cef970044678b234b
Fernando Henrique <liquuid@gmail.com>
2019-07-13 07:08:59 -0300

    User type

--- schema-query/index.js
+++ schema-query/index.js
@@ -5,15 +5,40 @@ const typeDefs = gql`
     type Query {
         hello: String
         dateNow: Date
+        loggedUser: User
+    }
+
+    type User{
+        id: ID
+        name: String!
+        email: String!
+        age: Int
+        wage: Float
+        vip: Boolean
     }
 `
 const resolvers = {
+    User: {
+        wage(user) {
+            return user.wage_usd
+        }
+    },
     Query: {
         hello() {
              return "Hi !!!"
         },
         dateNow() {
             return new Date;
+       },
+       loggedUser(){
+           return {
+               id: 1,
+               name: "asdf",
+               email: "asdf@sdf.com",
+               age: "66",
+               wage_usd: "666.66",
+               vip: true
+           }
        }
         
     }
