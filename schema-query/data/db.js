efd891e8b493a69bc1c1606eed3262439616a302
Fernando Henrique <liquuid@gmail.com>
2019-08-03 22:57:04 -0300

    status enum

--- schema-query/data/db.js
+++ schema-query/data/db.js
@@ -4,18 +4,21 @@ const users = [{
     email: "aaa@aaa.com",
     age: 29,
     role_id: 1,
+    status: 'ACTIVE',
 },{
     id: 2,
     name: "Bbbbbb Bbbbb",
     email: "bbb@bbb.com",
     age: 31,
     role_id: 1,
+    status: 'INACTIVE',
 },{
     id: 3,
     name: "Cccc Ccccc",
     email: "ccc@ccc.com",
     age: 32,
     role_id: 2,
+    status: 'BLOCKED',
 }]
 
 const roles = [
