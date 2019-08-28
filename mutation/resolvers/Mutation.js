906b4bffe501d0812cc5f640a33d05249be3d3cd
Fernando Henrique <liquuid@gmail.com>
2019-08-28 20:29:34 -0300

    alter users

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -25,5 +25,17 @@ module.exports = {
         if (i < 0) return null
         const excluded = users.splice(i, 1)
         return excluded ? excluded[0] : null
+    },
+
+    alterUser(_, args){
+        const i = users
+            .findIndex(u => u.id === args.id )
+        if (i < 0 ) return null
+        const user = {
+            ...users[i],
+            ...args
+        }
+        users.splice(i, 1, user)
+        return user
     }
 }
