da7d6db3b0f125013d55853de04cb36d541ccf94
Fernando Henrique <liquuid@gmail.com>
2019-08-28 20:59:54 -0300

    user filters

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -1,6 +1,21 @@
 const { users, nextID } =
     require('../data/db')
 
+function userIndex(filter){
+    if(!filter) return -1
+    const {id, email } = filter
+    if(id){
+        return users
+            .findIndex( u => u.id === id)
+
+    } else if(email) {
+        return users
+            .findIndex( u => u.email === email)
+    }
+
+}
+
+
 module.exports = {
     // { name, email, age })
     newUser(_, { data } ) {
@@ -19,9 +34,8 @@ module.exports = {
         users.push(novo)
         return novo
     },
-    removeUser(_, { id }){
-        const i = users
-            .findIndex(u => u.id === id)
+    removeUser(_, { filter }){
+        const i = userIndex(filter)
         if (i < 0) return null
         const excluded = users.splice(i, 1)
         return excluded ? excluded[0] : null
