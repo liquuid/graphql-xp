9a60b0594949315215c432927a49b672e609fba8
Fernando Henrique <liquuid@gmail.com>
2019-08-31 03:25:31 -0300

    fillup initial migrations

--- db/proj/migrations/20190831025734_roles.js
+++ db/proj/migrations/20190831025734_roles.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('roles', table => {
+      table.increments('id').primary()
+      table.string('name').notNull().unique()
+      table.string('label').notNull()
+  }).then( function() {
+      return knex('roles').insert([
+          { name:'common', label: 'Common'},
+          { name:'admin', label: 'Admin'},
+          { name:'master', label: 'Master'},
+      ])
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('roles')
 };
--- db/proj/migrations/20190831025913_users_roles.js
+++ db/proj/migrations/20190831025913_users_roles.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('users_roles', table => {
+        table.integer('user_id').unsigned()
+        table.integer('role_id').unsigned()
+        table.foreign('user_id').references('users.id')
+        table.foreign('role_id').references('roles.id')
+        table.primary(['user_id', 'role_id'])
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTable('users_roles')
+  };
